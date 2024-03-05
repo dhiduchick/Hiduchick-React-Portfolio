b8ba816cf4acd7e0da7b0d6800e69776be87ca94
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-03-04 21:00:27 -0500

    navigation index.js complete

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,24 @@
+const Navigation = ({currentPage, handlePageChange}) => {
+    const navLinks = ["About me", "Portfolio", "Contact", "Resume"];
+  
+    return (
+      <nav>
+        <ul className="nav nav-tabs">
+          {navLinks.map((link) => (
+            <li className="nav-item" key={link}>
+              <a href={"#" + link.toLowerCase()} 
+              onClick={() => {
+                handlePageChange(link); 
+              }}
+                className={currentPage === link ? "nav-link navActive" : "nav-link"}>
+                  {link}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    )
+  }
+  
+  export default Navigation;
+  
