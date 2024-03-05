939e1abdce9536c64b9487bb8a57af3f4faaa3b1
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-03-04 20:35:18 -0500

    footer complete

---
+++ src/components/Footer/index.js
@@ -0,0 +1,24 @@
+const Footer = () => {
+    return (
+      <footer>
+        <section className="footer-section">
+          <ul className="footer-links">
+            <li>
+                <a href="https://www.linkedin.com/in/daelyn-hiduchick-811a75232/" target="_blank" rel="noreferrer">
+                  <span className="icon icon-linkedin" aria-label="linkedin profile link"></span>
+                </a>
+            </li>
+            <li>
+                <a href="https://github.com/dhiduchick" target="_blank" rel="noreferrer">
+                  <span className="icon icon-github" aria-label="github profile link"></span>
+                </a>
+            </li>
+          </ul>
+          <p>Made by Daelyn Hiduchick.</p>
+        </section>
+      </footer>
+    )
+  }
+  
+  export default Footer;
+  
