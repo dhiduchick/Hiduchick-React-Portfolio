6c1b16a546368b9456e3e0821d7f5fabb3e880a2
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-03-04 21:33:18 -0500

    App.js complete

---
+++ src/App.js
@@ -0,0 +1,39 @@
+// import logo from './logo.svg';
+import { useState } from "react";
+import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+import Footer from './components/Footer';
+import Portfolio from './components/Portfolio';
+import ContactForm from "./components/Contact";
+import Resume from "./components/Resume";
+
+function App() {
+  const [currentPage, handlePageChange] = useState("About me");
+
+  const renderPage = () => {
+    switch(currentPage) {
+      case "About me":
+        return <About />;
+      case "Portfolio":
+        return <Portfolio />;
+      case "Contact":
+        return <ContactForm />;
+      case "Resume":
+        return <Resume />;
+      default: 
+        return <About />;
+    }
+  };
+
+  return (
+    <div>
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer />
+    </div>
+    
+  );
+}
+
+export default App;
