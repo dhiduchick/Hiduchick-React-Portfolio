8c59b22af1d12ffbfed2b892843a11bc2bbffec5
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-03-04 20:57:10 -0500

    header index.js complete

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import Navigation from "../Navigation";
+
+const Header = (props) => {
+  return (
+    <header>
+      <div className="header-block">
+        <div className="dev-name">Daelyn Hiduchick</div>
+            <div className="subhead">Full Stack Web Developer</div>
+      </div>
+      <Navigation 
+        currentPage={props.currentPage}
+        handlePageChange={props.handlePageChange}
+      />
+    </header>
+  )
+}
+
+export default Header;
