60a0672b1be91fdb7c288ecec64041e435716997
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-03-06 19:47:34 -0500

    nav complete

---
+++ src/components/Nav.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Navigation() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: 'black', position: 'sticky', top: '0', zIndex: '100' }}>
+            <div className="container" style={{ backgroundColor: 'black' }}>
+                <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" style={{ backgroundColor: 'black', borderColor: 'white' }}></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link text-white" style={{ backgroundColor: 'black' }} to="/">About Me</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-white" style={{ backgroundColor: 'black' }} to="/portfolio">Portfolio</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-white" style={{ backgroundColor: 'black' }} to="/contact">Contact</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-white" style={{ backgroundColor: 'black' }} to="/resume">Resume</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+
+export default Navigation
