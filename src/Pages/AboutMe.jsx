57cbcbba49a050ff0847099032a16ce2c1381493
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-03-06 20:03:18 -0500

    about me complete

---
+++ src/Pages/AboutMe.jsx
@@ -0,0 +1,20 @@
+function AboutMe() {
+    return (
+        <div className="contact-form-section  text-center rounded-lg p-4">
+            <h1>About Me</h1>
+
+
+            <img src="./public/F6BFEFED-856A-4423-9EC4-E8D0506C7567_1_105_c.jpeg" alt="Profile picture of Dylan Coito" className="img-fluid rounded my-3" style={{ maxWidth: '250px' }} />
+            <p className="contact-form-section  p-4">
+                My name is Daelyn, and I am a Full-Stack Software Engineer. I specialize in JavaScript, with a focus on developing React applications. 
+                Originally from Massachusetts, I am currently looking to relocate to Florida. Before transitioning into the field of software development, 
+                I worked in the STEM field as a Medical Technologist at a private lab doing food sensitivty testing. I have a BS from Roger Williams University with a degree in Forensic Science.
+                In my free time, I enjoy reading, drawing, and paintings. I am thrilled to embark on this journey in the tech industry and create amazing applications. 
+                Thank you for visiting my portfolio! For more details about my experience or tech stack, please take a look at my resume. 
+            </p>
+        </div>
+    )
+}
+
+
+export default AboutMe;
