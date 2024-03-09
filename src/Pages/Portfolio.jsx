024e6d201a4d1c9db09f38fceafec1b6aee34e43
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-03-09 14:06:49 -0500

    fixed image paths

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -13,49 +13,49 @@ function Portfolio() {
     {
       title: 'Culinary Canvas',
       description: '',
-      imageUrl: '../public/homepageculinary.png',
+      imageUrl: '/homepageculinary.png',
       projectUrl: 'https://evening-reef-48573-57db96f3f837.herokuapp.com/'
     },
     {
         title: 'Note Taker',
         description: '',
-        imageUrl: '../public/notetaker.png',
+        imageUrl: '/notetaker.png',
         projectUrl: 'https://github.com/dhiduchick/Hiduchick-Note-Taker.git'  
     },
     {
         title: 'Password Generator',
         description: '',
-        imageUrl: '../public/passwordgen.png',
+        imageUrl: '/passwordgen.png',
         projectUrl: 'https://dhiduchick.github.io/Hiduchick-Password-Generator/'
       },
       {
         title: 'README Generator',
         description: '',
-        imageUrl: '../public/readme-preview.png',
+        imageUrl: '/readme-preview.png',
         projectUrl: 'https://github.com/dhiduchick/Hiduchick-README-Generator.git'
       },
       {
         title: 'Cosmic Counsel',
         description: '',
-        imageUrl: '../public/StartPageCC.png',
+        imageUrl: '/StartPageCC.png',
         projectUrl: 'https://conartisttt.github.io/Cosmic-Counsel/'
       },
       {
         title: 'Weather Dashbord',
         description: '',
-        imageUrl: '../public/weatherdash.png',
+        imageUrl: '/weatherdash.png',
         projectUrl: 'https://dhiduchick.github.io/Hiduchick-Weather-Dashboard/'
       },
       {
         title: 'Coding Quiz',
         description: '',
-        imageUrl: '../public/codingquiz.png',
+        imageUrl: '/codingquiz.png',
         projectUrl: 'https://dhiduchick.github.io/Hiduchick-Coding-Quiz/'
       },
       {
         title: 'Work Day Scheduler',
         description: '',
-        imageUrl: '../public/workday.png',
+        imageUrl: '/workday.png',
         projectUrl: 'https://dhiduchick.github.io/Hiduchick-Day-Scheduler/'
     }
   ];
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ function Footer() {
     <footer className="text-center rounded-lg p-0" style={{ position: 'fixed', bottom: 0, left: 0, width: '100%', paddingTop:10 }}>
       <div className="text-center rounded-lg p-0" style={{ position: 'fixed', bottom: 0, left: 0, width: '100%'}}>
         <a href="https://www.linkedin.com/in/daelyn-hiduchick-811a75232/" className="text-white me-3" target="_blank" rel="noopener noreferrer">
-          <img src="./public/linkedin.png" alt="Linkedin Icon" style={{ width: "50px", height: "50px" }} />
+          <img src="/linkedin.png" alt="Linkedin Icon" style={{ width: "50px", height: "50px" }} />
         </a>
         <a href="https://github.com/dhiduchick" className="text-white" target="_blank" rel="noopener noreferrer">
-          <img src="./public/github.png" alt="GitHub Icon" style={{ width: "50px", height: "50px" }} />
+          <img src="/github.png" alt="GitHub Icon" style={{ width: "50px", height: "50px" }} />
         </a>
       </div>
     </footer>
