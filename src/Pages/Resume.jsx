487ff906caa104f79b3b221feedc7b6b645f51a0
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-03-09 14:21:58 -0500

    view resume instead of download

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -7,7 +7,7 @@ const Resume = () => {
   return (
     <div style={{ marginBottom: '150px' }}>
       <h1 className="contact-form-section text-center rounded-lg p-4">My Resume</h1>
-      <h3 className="contact-form-section rounded-pill text-center rounded-lg p-4"style={{ backgroundColor: '#c7e2bd' }}>Download my resume <a href="https://docs.google.com/document/d/1XAESBFieLCd4egDw3Q4K-Hg0Ujze49eKaJ9PrJDEjp8/edit?usp=sharing" style={{ backgroundColor: '#c7e2bd' }}>✧*̥˚ Here *̥˚✧</a></h3>
+      <h3 className="contact-form-section rounded-pill text-center rounded-lg p-4"style={{ backgroundColor: '#c7e2bd' }}>View Full Resume <a href="https://docs.google.com/document/d/1XAESBFieLCd4egDw3Q4K-Hg0Ujze49eKaJ9PrJDEjp8/edit?usp=sharing" style={{ backgroundColor: '#c7e2bd' }}>✧*̥˚ Here *̥˚✧</a></h3>
 
       <section className="contact-form-section rounded-lg p-4">
         <h3>Front-End Technologies:</h3>
